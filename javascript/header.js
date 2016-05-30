e87a31bae2b81103b93e4f0e446802a7b4726f2d
kimkm <keikeiem@aimtog.co.kr>
2016-05-30 13:09:11 +0900

    Common Method  - compareArray 추가

--- javascript/header.js
+++ javascript/header.js
@@ -3,6 +3,29 @@ var Matrix = {
 
 	},
 	summation: function(matrixA, matrixB) {
-		this.getSize
+		if (Common.compareArray(matrixA, matrixB)) {
+			console.log('Do it');
+		}
+	}
+};
+
+var Common = {
+	// This function is used to check given two arrays are whether equal or not.
+	compareArray: function(arrayA, arrayB) {
+		// If either arrayA or arrayB is undefined, return false.
+		if (!arrayA || !arrayB) {
+			return false;
+		}
+		// If arrayA and arrayB have different length, return false.
+		if (arrayA.length !== arrayB.length) {
+			return false;
+		}
+		var compareStatement = true;
+		for (var i = 0; i < arrayA.length; i++) {
+			if (arrayA[i] !== arrayB[i]) {
+				compareStatement = false;
+			}
+		}
+		return compareStatement;
 	}
 };
