1c9a4b5b409b15e2fdfc7d6c87b1705c34e2e347
unknown <>
2016-06-02 00:45:05 +0900

    -

--- htdocs/js/matrices.js
+++ htdocs/js/matrices.js
@@ -1,5 +1,16 @@
 function inputData() {
-	var test = document.getElementById('console').value;
+	if (document.getElementById('matrix_1')) {
+		var firstMatrix = document.getElementById('matrix_1').value;
+		if (firstMatrix) {
+			firstMatrix = parseMatrixString(firstMatrix);
+		}
+	}
+	if (document.getElementById('matrix_2')) {
+		var secondMatrix = document.getElementById('matrix_2').value;
+		if (secondMatrix) {
+			secondMatrix = parseMatrixString(secondMatrix);
+		}
+	}
 	test = test.split('[')[1];
 	test = test.split(']')[0];
 	test = test.split(';');
@@ -10,4 +21,14 @@ function inputData() {
 	}
 	console.log(rank);
 	console.log(test);
+}
+
+function parseMatrixString(stringValue) {
+	if (typeof stringValue !== 'string') {
+		return;
+	}
+	var particle = stringValue.split('[')[1];
+	particle = particle.split(']')[0];
+	particle = particle.split(';');
+
 }
