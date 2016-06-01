0b3dd0647d7f3dca6c7d8aa66a478261e24eed42
kimkm <keikeiem@aimtog.co.kr>
2016-06-01 19:55:32 +0900

    -

--- htdocs/js/matrices.js
+++ htdocs/js/matrices.js
@@ -1,4 +1,13 @@
 function inputData() {
 	var test = document.getElementById('console').value;
+	test = test.split('[')[1];
+	test = test.split(']')[0];
+	test = test.split(';');
+	var rows = test.length;
+	var rank = [];
+	for (var i = 0; i < rows; i++) {
+		rank[i] = test[i].split(',').length;
+	}
+	console.log(rank);
 	console.log(test);
 }
